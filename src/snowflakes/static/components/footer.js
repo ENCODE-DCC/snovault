912a05ca6b212ca1155dd04367aab128b49fec4b
j1z0 <jeremy@softworks.com.my>
2016-07-19 11:37:30 -0400

    basic popup box courtesy of @carlvitzthum

--- src/snowflakes/static/components/footer.js
+++ src/snowflakes/static/components/footer.js
@@ -1,5 +1,7 @@
 'use strict';
 var React = require('react');
+var Modal = require('react-bootstrap/lib/Modal');
+var OverlayMixin = require('react-bootstrap/lib/OverlayMixin');
 
 var Footer = React.createClass({
     contextTypes: {
@@ -16,7 +18,7 @@ var Footer = React.createClass({
         var userActionRender;
 
         if (!(session && session['auth.userid'])) {
-            userActionRender = <a href="#" data-trigger="login" disabled={disabled}>Submitter sign-in</a>;
+					 userActionRender = <LoginBoxes/>
         } else {
             userActionRender = <a href="#" data-trigger="logout">Submitter sign out</a>;
         }
@@ -52,6 +54,92 @@ var Footer = React.createClass({
             </footer>
         );
     }
+});
+
+// LoginBox Popup
+var LoginBoxes = React.createClass({
+  mixins: [OverlayMixin],
+  getInitialState: function() {
+  	return {username: '', password: '', isOpen: false};
+  },
+  usernameFill: function(v) {
+  	this.setState({username: v});
+  },
+  passwordFill: function(v) {
+  	this.setState({password: v});
+  },
+  handleToggle: function () {
+	  this.setState({
+		  isOpen: !this.state.isOpen
+	  });
+  },
+  handleSubmit: function(e){
+    e.preventDefault();
+    var username = this.state.username.trim();
+	var password = this.state.password.trim();
+  	if (username === '' || password === '') {
+    	return;
+    }
+    // do something
+    this.setState({username: '', password: ''});
+    console.log('EXIT THE PAGE NOW');
+  },
+  render: function () {
+	return (
+	  <div>
+		  <a id="loginbtn" onClick={this.handleToggle}>Log in</a>
+	 </div>
+ );
+ },
+
+	 renderOverlay: function () {
+         if (!this.state.isOpen) {
+             return <span/>;
+         }
+         return (
+	  <Modal onRequestHide={this.handleToggle} dialogClassName="login-modal">
+        <div className="login-box">
+        	<h1 className="title">Account Login</h1>
+      		<label className="fill-label">Username:</label>
+        	<TextBox default="Username" fill={this.usernameFill} tType="text"/>
+        	<label className="fill-label">Password:</label>
+        	<TextBox default="Password" fill={this.passwordFill} tType="password"/>
+        	<ul className="links">
+            	<li><button id="popuploginbtn" className="sexy-btn"
+	                onClick={this.handleSubmit}><span>Sign in</span></button></li>
+				<li><form action='http://google.com'><button id="regbtn" className="sexy-btn">
+					<span>Register</span></button></form></li>
+				<li><form action='http://google.com'><button id="passbtn" className="sexy-btn">
+					<span>Change password</span></button></form></li>
+				{/*<li><a href="https://www.google.com/">Register</a></li>
+	            <li><a href="https://www.google.com/">New password</a></li>*/}
+	            <li><button id="closebtn" className="sexy-btn"
+	                onClick={this.handleToggle}><span>Close</span></button></li>
+        	</ul>
+      	</div>
+  	   </Modal>
+    );
+  },
+});
+
+var TextBox = React.createClass({
+  getInitialState: function() {
+  	return({data: ''});
+  },
+  handleFill: function(e) {
+
+  	this.setState({data: e.target.value});
+    this.props.fill(e.target.value);
+  },
+  render: function() {
+  	return(
+    	<div>
+    		<input type={this.props.tType} className="text-box"
+    		placeholder={this.props.default} onChange={this.handleFill}
+    		value={this.state.data} />
+    	</div>
+  	);
+  },
 });
 
 module.exports = Footer;
