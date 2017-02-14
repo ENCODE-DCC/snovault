7a9383a1dbf4d4995bab3aebdbb48fd8f1617189
Ben Hitz <hitz@stanford.edu>
2017-02-14 10:09:09 -0800

    version bump

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,1 +1,4 @@
 0.20 - Patch mpindexer for better error messages
+0.21 - (pypi errors, identical to 0.22)
+0.22 - New version of image magic, fix sauce labs
+0.23 - replace copy.deepcopy() for faster indexing
--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ tests_require = [
 
 setup(
     name='snovault',
-    version='0.22',
+    version='0.23',
     description='Snovault Hybrid Object Relational Database Framework',
     long_description=README + '\n\n' + CHANGES,
     packages=find_packages('src'),
