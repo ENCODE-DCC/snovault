714298dfb0cc50029ed5213d9f1ca1845a3b2911
j1z0 <jeremy@softworks.com.my>
2017-03-06 14:57:34 -0500

    added proxy support for apache with attachemnts

--- src/snovault/attachment.py
+++ src/snovault/attachment.py
@@ -5,6 +5,7 @@ from mimetypes import guess_type
 from PIL import Image
 from pyramid.httpexceptions import (
     HTTPNotFound,
+    HTTPFound
 )
 from pyramid.response import Response
 from pyramid.traversal import find_root
@@ -207,7 +208,9 @@ def download(context, request):
     blob_storage = request.registry[BLOBS]
     if hasattr(blob_storage, 'get_blob_url'):
         blob_url = blob_storage.get_blob_url(download_meta)
-        return Response(headers={'X-Accel-Redirect': '/_proxy/' + str(blob_url)})
+        # we don't use nginx in production
+        # return Response(headers={'X-Accel-Redirect': '/_proxy/' + str(blob_url)})
+        raise HTTPFound(location=str(blob_url))
 
     # Otherwise serve the blob data ourselves
     blob = request.registry[BLOBS].get_blob(download_meta)
