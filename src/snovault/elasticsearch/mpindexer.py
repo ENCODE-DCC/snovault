de2339e9c0d9b273bbe060598351150a1f15d4e1
Tim Dreszer <tdreszer@stanford.org>
2017-05-11 10:04:05 -0700

    Chunksize should stay at 32, as that is what has been tested.

--- src/snovault/elasticsearch/mpindexer.py
+++ src/snovault/elasticsearch/mpindexer.py
@@ -110,7 +110,7 @@ def update_object_in_snapshot(args):
 # Running in main process
 
 class MPIndexer(Indexer):
-    chunksize = 1
+    chunksize = 32
     maxtasks = 4  # pooled processes will exit and be replaced after this many tasks are completed.
 
     def __init__(self, registry, processes=None):
