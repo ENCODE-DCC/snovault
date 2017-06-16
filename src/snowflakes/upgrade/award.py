74b8f84905dffa9b4f19f5dd62c2fbf355e03c40
Ben Hitz <hitz@stanford.edu>
2017-06-16 10:11:19 -0700

    add basic audits tests

--- src/snowflakes/upgrade/award.py
+++ src/snowflakes/upgrade/award.py
@@ -5,8 +5,7 @@ from snovault import upgrade_step
 
 @upgrade_step('award', '', '2')
 def award_0_2(value, system):
-    # http://redmine.encodedcc.org/issues/1295
-    # http://redmine.encodedcc.org/issues/1307
+    # Sample upgrades with tests
 
     rfa_mapping = ['ENCODE2', 'ENCODE2-Mouse']
     if value['rfa'] in rfa_mapping:
@@ -14,7 +13,6 @@ def award_0_2(value, system):
     else:
         value['status'] = 'current'
 
-    # http://encode.stanford.edu/issues/1022
     if 'url' in value:
         if value['url'] == '':
             del value['url']
@@ -22,7 +20,6 @@ def award_0_2(value, system):
 
 @upgrade_step('award', '2', '3')
 def award_2_3(value, system):
-    # http://redmine.encodedcc.org/issues/4743
 
     if value['viewing_group'] == 'ENCODE':
         value['viewing_group'] = 'ENCODE3'
