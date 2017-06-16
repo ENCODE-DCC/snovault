6f4919754b5d636907ac85862bd0172f64a15420
j1z0 <jeremy@softworks.com.my>
2017-06-16 12:47:55 -0400

    try with localhost

--- src/snovault/tests/serverfixtures.py
+++ src/snovault/tests/serverfixtures.py
@@ -66,6 +66,7 @@ def elasticsearch_host_port():
 def elasticsearch_server(request, elasticsearch_host_port):
     from .elasticsearch_fixture import server_process
     host, port = elasticsearch_host_port
+    host = 'localhost'
     port = 9200
     tmpdir = request.config._tmpdirhandler.mktemp('elasticsearch', numbered=True)
     tmpdir = str(tmpdir)
