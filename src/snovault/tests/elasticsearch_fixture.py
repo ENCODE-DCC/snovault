dd3211b15b4ecb9de58e381e56330b1f42180c5b
j1z0 <jeremy@softworks.com.my>
2017-06-16 10:55:01 -0400

    travis please do me a solid

--- src/snovault/tests/elasticsearch_fixture.py
+++ src/snovault/tests/elasticsearch_fixture.py
@@ -29,9 +29,13 @@ def server_process(datadir, host='127.0.0.1', port=9200, prefix='', echo=False):
         '-Enetwork.host=%s' % host,
         '-Ehttp.port=%d' % port,
     ]
-    # elasticsearch.deb setup
-    if os.path.exists('/etc/elasticsearch'):
+    # elasticsearch for travis
+    if os.environ.get('TRAVIS'):
+        args.append('-Epath.conf=%s/deploy' % os.environ['TRAVIS_BUILD_DIR'])
+    elif os.path.exists('/etc/elasticsearch'):
+        # elasticsearch.deb setup
        args.append('-Epath.conf=/etc/elasticsearch')
+
     process = subprocess.Popen(
         args,
         close_fds=True,
