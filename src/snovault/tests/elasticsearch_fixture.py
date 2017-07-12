2bbccef750674d1a734a9d6bfb101cdd85b4d211
Ulugbek Baymuradov <ulugbekbk@gmail.com>
2017-07-12 13:36:53 -0700

    ff

--- src/snovault/tests/elasticsearch_fixture.py
+++ src/snovault/tests/elasticsearch_fixture.py
@@ -14,7 +14,7 @@ def server_process(datadir, host='127.0.0.1', port=9200, prefix='', echo=False):
     ]
     if os.environ.get('TRAVIS'):
         echo=True
-        args.append('-Epath.conf=/etc/elasticsearch')
+        args.append('-Epath.conf=%s/conf' % os.environ['TRAVIS_BUILD_DIR'])
     elif os.path.exists('/etc/elasticsearch'):
         # elasticsearch.deb setup
         args.append('-Epath.conf=/etc/elasticsearch')
