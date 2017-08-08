70bcee58966f3f61ad29b1c1aed2a007532af5b6
Carl Vitzthum <carl.vitzthum@gmail.com>
2017-08-08 11:32:56 -0400

    fourfront_utils test fix

--- src/snowflakes/tests/test_fourfront_utils.py
+++ src/snowflakes/tests/test_fourfront_utils.py
@@ -2,7 +2,7 @@ import pytest
 
 def test_get_jsonld_types_from_collection_type(app):
     from snovault.fourfront_utils import get_jsonld_types_from_collection_type
-    test_item_type = 'snowset'
-    expected_types = ['snowball', 'snowset']
+    test_item_type = 'Snowset'
+    expected_types = ['snowball', 'snowfort']
     res = get_jsonld_types_from_collection_type(app, test_item_type)
-    assert res.sort() == expected_types
+    assert sorted(res) == expected_types
