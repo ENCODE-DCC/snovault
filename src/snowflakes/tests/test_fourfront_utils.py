8209ca718d259ea0cdc75ee2847f6eaa01190041
Carl Vitzthum <carl.vitzthum@gmail.com>
2017-11-08 10:58:07 -0500

    Worked on fourfront utils unit tests

--- src/snowflakes/tests/test_fourfront_utils.py
+++ src/snowflakes/tests/test_fourfront_utils.py
@@ -12,9 +12,9 @@ def test_build_default_embeds():
     from snovault.fourfront_utils import build_default_embeds
     """ simple unit test """
     embeds_to_add = ['obj1.obj2', 'obj1', 'obj1.obj3.*']
-    processed_embeds = ['obj1.*']
-    final_embs = build_default_embeds(embeds_to_add, processed_embeds)
-    expected_embs = [
+    processed_embeds = {'obj1.*'}
+    final_embeds = build_default_embeds(embeds_to_add, processed_embeds)
+    expected_embeds = [
         'obj1.obj2.link_id',
         'obj1.obj2.display_title',
         'obj1.obj2.@id',
@@ -23,12 +23,20 @@ def test_build_default_embeds():
         'obj1.*',
         'obj1.obj3.*'
     ]
-    assert(set(final_embs) == set(expected_embs))
+    assert(set(final_embeds) == set(expected_embeds))
 
-def test_find_default_embeds_for_schema(app):
-    from snovault.fourfront_utils import find_default_embeds_for_schema
+
+def test_find_default_embeds_and_expand_emb_list(registry):
+    from snovault.fourfront_utils import find_default_embeds_for_schema, expand_embedded_list
     from snovault import TYPES
-    type_info = registry[TYPES].by_item_type['snowset']
+    # use snowflake as test case
+    type_info = registry[TYPES].by_item_type['snowflake']
     schema_props = type_info.schema.get('properties')
     default_embeds = find_default_embeds_for_schema('', schema_props)
-    assert(True)
+    expected_embeds = ['lab', 'award', 'submitted_by', 'snowset', 'principals_allowed.*']
+    assert(set(default_embeds) == set(expected_embeds))
+    # lets use the default embeds as an "embedded_list" for snowflake
+    dummy_emb_list = [emb + '.*' if not emb.endswith('*') else emb for emb in expected_embeds ]
+    embs_to_add, proc_embs = expand_embedded_list('snowflake', registry[TYPES], dummy_emb_list, schema_props, set())
+    expected_to_add = ['lab.pi', 'lab.awards', 'lab.principals_allowed.*', 'award.pi', 'award.principals_allowed.*', 'submitted_by.lab', 'submitted_by.submits_for', 'submitted_by.principals_allowed.*', 'snowset.lab', 'snowset.award', 'snowset.principals_allowed.*', 'snowset.submitted_by']
+    assert(set(embs_to_add) == set(expected_to_add))
