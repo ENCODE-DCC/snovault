fb4056aacfd54301c85192e0222950bc9f8d686a
Carl Vitzthum <carl.vitzthum@gmail.com>
2018-03-22 10:05:36 -0400

    Add no-deps arg to pip

--- bootstrap.py
+++ bootstrap.py
@@ -62,9 +62,9 @@ else:
 ######################################################################
 # Install buildout
 if options.buildout_version is not None:
-    pip.main(['install', '--upgrade', 'zc.buildout==%s' % options.buildout_version])
+    pip.main(['install', '--no-deps', '--upgrade', 'zc.buildout==%s' % options.buildout_version])
 else:
-    pip.main(['install', '--upgrade', 'zc.buildout'])
+    pip.main(['install', '--no-deps', '--upgrade', 'zc.buildout'])
 
 ######################################################################
 # Import and run buildout
