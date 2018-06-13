42ee253751809a9409e5e78b3f62a01eac939738
Alexander Balashov <alexander_balashov@hms.harvard.edu>
2018-06-13 13:29:41 -0400

    Update crud_views.py

--- src/snovault/crud_views.py
+++ src/snovault/crud_views.py
@@ -236,7 +236,7 @@ def item_edit(context, request, render=None):
 @view_config(context=Item, permission='edit', request_method='DELETE', decorator=if_match_tid)
 def item_delete_full(context, request, render=None):
     # possibly temporary fix to check if user is admin
-    if hasattr(reqeust, 'user_info'):
+    if hasattr(request, 'user_info'):
         user_details = request.user_info.get('details', {})
     else:
         from pyramid.security import effective_principals
