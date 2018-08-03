d365dc65408c667034fda606d8caf30f33f6468a
caseylitton <caseylitton@gmail.com>
2018-08-03 15:27:12 -0700

    [REVERT] short invalided for indexing to 100

--- src/snovault/elasticsearch/indexer.py
+++ src/snovault/elasticsearch/indexer.py
@@ -246,7 +246,7 @@ def index(request):
                     snapshot_id = connection.execute('SELECT pg_export_snapshot();').scalar()
 
     if invalidated and not dry_run:
-
+        invalidated = invalidated[:100]
         if len(stage_for_followup) > 0:
             # Note: undones should be added before, because those uuids will (hopefully) be indexed in this cycle
             state.prep_for_followup(xmin, invalidated)
