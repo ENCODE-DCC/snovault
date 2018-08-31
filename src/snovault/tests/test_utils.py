a07834d3f88979b28dcb286017e8524d989b7f85
Keenan Graham <keenangraham@stanford.edu>
2018-08-31 14:41:59 -0700

    Add tests for _get_calculated_properties_from_paths

---
+++ src/snovault/tests/test_utils.py
@@ -0,0 +1,33 @@
+import pytest
+
+from snovault.util import (
+    select_distinct_values,
+    _get_calculated_properties_from_paths,
+)
+
+
+COLLECTION_URL = '/testing-link-targets/'
+
+
+def test_get_calculated_properties_from_paths(dummy_request):
+    paths = [
+        COLLECTION_URL + 'ABC123/'
+    ]
+    calculated_properties = _get_calculated_properties_from_paths(dummy_request, paths)
+    assert 'reverse' in calculated_properties
+
+
+def test_not_a_collection_get_calculated_properties_from_paths(dummy_request):
+    paths = [
+        '/yxa/ABC123/'
+    ]
+    calculated_properties = _get_calculated_properties_from_paths(dummy_request, paths)
+    assert not calculated_properties
+
+
+def test_unformed_path_get_calculated_properties_from_paths(dummy_request):
+    paths = [
+        'testing-link-targets/ABC123/'
+    ]
+    calculated_properties = _get_calculated_properties_from_paths(dummy_request, paths)
+    assert not calculated_properties
