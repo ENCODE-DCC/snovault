1ddeb05e3b258cdcc52390ba75a959b875856b84
Casey Litton <caseylitton@gmail.com>
2018-09-06 16:09:53 -0700

    Update to version 1.0.16 (#133)

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ tests_require = [
 
 setup(
     name='snovault',
-    version='1.0.15',
+    version='1.0.16',
     description='Snovault Hybrid Object Relational Database Framework',
     long_description=README + '\n\n' + CHANGES,
     packages=find_packages('src'),
