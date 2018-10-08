15083f3791f74045d867d5afd80b806fa9f25da5
caseylitton <caseylitton@gmail.com>
2018-10-08 12:04:23 -0700

    Organize primary indexer

--- src/snovault/elasticsearch/primary_indexer.py
+++ src/snovault/elasticsearch/primary_indexer.py
@@ -2,14 +2,13 @@
 import datetime
 import logging
 import time
-import json
 
 from elasticsearch.exceptions import (
-    ConflictError,
+    ConflictError as ESConflictError,
     ConnectionError as ESConnectionError,
-    TransportError,
+    TransportError as ESTransportError,
 )
-from sqlalchemy.exc import StatementError
+from sqlalchemy.exc import StatementError as SQLStatementError
 from urllib3.exceptions import ReadTimeoutError
 
 from snovault import STORAGE
@@ -18,6 +17,78 @@ from .interfaces import ELASTIC_SEARCH
 
 
 log = logging.getLogger(__name__)  # pylint: disable=invalid-name
+
+
+class IndexItem(object):
+    '''Container for item to index'''
+    # pylint: disable=too-many-instance-attributes
+    def __init__(self, request, uuid, xmin):
+        self._backoff_errors = {}
+        self._embed_doc = {}
+        self._embed_url = '/{uuid}/@@index-data/'.format(uuid=uuid)
+        self.error = None
+        self.request = request
+        self._uuid = str(uuid)
+        self._xmin = xmin
+
+    def _add_error(self, ecp, backoff=0):
+        timestamp = datetime.datetime.now().isoformat()
+        error = {
+            'error_message': repr(ecp),
+            'uuid': self._uuid,
+            'timestamp': timestamp,
+        }
+        if backoff:
+            self._backoff_errors[backoff] = error
+        else:
+            self.error = error
+
+    def index_item(self, registry_es):
+        '''Attempt to index uuid with embed doc as body'''
+        if not self._embed_doc or self.error:
+            return
+        for backoff in [0, 10, 20, 40, 80]:
+            time.sleep(backoff)
+            try:
+                registry_es.index(
+                    index=self._embed_doc['item_type'],
+                    doc_type=self._embed_doc['item_type'],
+                    body=self._embed_doc,
+                    id=self._uuid,
+                    version=self._xmin,
+                    version_type='external_gte',
+                    request_timeout=30,
+                )
+            except SQLStatementError as ecp:
+                log.error('SQL Error rendering %s', self._embed_url, exc_info=True)
+                self._add_error(ecp, backoff=backoff)
+                # Can't reconnect until invalid transaction is rolled back
+                raise ecp
+            except ESConflictError as ecp:
+                log.warning('Conflict indexing %s at version %d', self._uuid, self._xmin)
+                self._add_error(ecp, backoff=backoff)
+                break
+            except (ESConnectionError, ReadTimeoutError, ESTransportError) as ecp:
+                log.warning('Retryable error indexing %s: %r', self._uuid, ecp)
+                self._add_error(ecp, backoff=backoff)
+            except Exception as ecp:  # pylint: disable=broad-except
+                log.error('Error indexing %s', self._uuid, exc_info=True)
+                self._add_error(ecp, backoff=backoff)
+                break
+            break
+
+    def request_embed(self):
+        '''Get embedded for uuid'''
+        try:
+            self._embed_doc = self.request.embed(self._embed_url, as_user='INDEXER')
+        except SQLStatementError as ecp:
+            log.error('SQL Error rendering %s', self._embed_url, exc_info=True)
+            self._add_error(ecp)
+            # Can't reconnect until invalid transaction is rolled back
+            raise ecp
+        except Exception as ecp:  # pylint: disable=broad-except
+            log.error('Error rendering %s', self._embed_url, exc_info=True)
+            self._add_error(ecp)
 
 
 class PrimaryIndexer(object):
@@ -32,54 +103,21 @@ class PrimaryIndexer(object):
         '''Wapper to iterate over update_object'''
         # pylint: disable=too-many-arguments, unused-argument
         errors = []
-        for i, uuid in enumerate(uuids):
-            error = self.update_object(request, uuid, xmin)
-            if error is not None:
-                errors.append(error)
-            if (i + 1) % 50 == 0:
-                log.info('Indexing %d', i + 1)
+        for cnt, uuid in enumerate(uuids):
+            index_item = IndexItem(request, uuid, xmin)
+            self.update_object(index_item, restart=restart)
+            if index_item.error is not None:
+                errors.append(index_item.error)
+            if (cnt + 1) % 50 == 0:
+                log.info('Indexing %d', cnt + 1)
         return errors
 
-    def update_object(self, request, uuid, xmin, restart=False):
+    def update_object(self, index_item, restart=False):
         '''Gets embedded doc and indexes uuid'''
-        request.datastore = 'database'  # required by 2-step indexer
-        last_exc = None
-        try:
-            doc = request.embed('/%s/@@index-data/' % uuid, as_user='INDEXER')
-        except StatementError:
-            # Can't reconnect until invalid transaction is rolled back
-            raise
-        except Exception as ecp:  # pylint: disable=broad-except
-            log.error('Error rendering /%s/@@index-data', uuid, exc_info=True)
-            last_exc = repr(ecp)
-
-        if last_exc is None:
-            for backoff in [0, 10, 20, 40, 80]:
-                time.sleep(backoff)
-                try:
-                    self.registry_es.index(
-                        index=doc['item_type'], doc_type=doc['item_type'], body=doc,
-                        id=str(uuid), version=xmin, version_type='external_gte',
-                        request_timeout=30,
-                    )
-                except StatementError:
-                    # Can't reconnect until invalid transaction is rolled back
-                    raise
-                except ConflictError:
-                    log.warning('Conflict indexing %s at version %d', uuid, xmin)
-                    return
-                except (ESConnectionError, ReadTimeoutError, TransportError) as ecp:
-                    log.warning('Retryable error indexing %s: %r', uuid, ecp)
-                    last_exc = repr(ecp)
-                except Exception as ecp:  # pylint: disable=broad-except
-                    log.error('Error indexing %s', uuid, exc_info=True)
-                    last_exc = repr(ecp)
-                    break
-                else:
-                    # Get here on success and outside of try
-                    return
-        timestamp = datetime.datetime.now().isoformat()
-        return {'error_message': last_exc, 'timestamp': timestamp, 'uuid': str(uuid)}
+        # pylint: disable=unused-argument,
+        index_item.request.datastore = 'database'
+        index_item.request_embed()
+        index_item.index_item(self.registry_es)
 
     def shutdown(self):
         '''Shut down - Not used in Indexer'''
