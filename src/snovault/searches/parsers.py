3184f8aea30ee6e10ed816660a3c738e1c870cfa
Keenan Graham <keenangraham@stanford.edu>
2019-06-10 18:17:15 -0700

    Add query string filter

--- src/snovault/searches/parsers.py
+++ src/snovault/searches/parsers.py
@@ -8,3 +8,9 @@ class ParamsParser():
     def __init__(self, request):
         self._request = request
 
+    def get_filters_by_condition(self, key_condition=None, value_condition=None):
+        return [
+            (k, v) for k, v in self._request.params.items()
+            if key_condition is None or k == key_condition
+            and value_condition is None or v == value_condition
+        ]
---
+++ src/snovault/tests/test_searches_parsers.py
@@ -0,0 +1,65 @@
+import pytest
+from pyramid.security import effective_principals
+
+
+def test_searches_params_parser_init(dummy_request):
+    from snovault.searches.parsers import ParamsParser
+    p = ParamsParser(dummy_request)
+    assert isinstance(p, ParamsParser)
+    assert p._request is dummy_request
+
+
+def test_searches_params_parser_query_string(dummy_request):
+    from snovault.searches.parsers import ParamsParser
+    dummy_request.environ['QUERY_STRING'] = 'type=Experiment'
+    p = ParamsParser(dummy_request)
+    assert 'type' in p._request.params
+    assert p._request.params.getall('type') == ['Experiment']
+
+
+def test_searches_params_parser_query_string_not(dummy_request):
+    from snovault.searches.parsers import ParamsParser
+    dummy_request.environ['QUERY_STRING'] = 'type!=Experiment'
+    p = ParamsParser(dummy_request)
+    assert 'type!' in p._request.params
+
+
+def test_searches_params_parser_get_filters_by_condition_none(dummy_request):
+    from snovault.searches.parsers import ParamsParser
+    dummy_request.environ['QUERY_STRING'] = 'type=Experiment&type=File&field=status'
+    p = ParamsParser(dummy_request)
+    assert p.get_filters_by_condition() == [
+        ('type', 'Experiment'),
+        ('type', 'File'),
+        ('field', 'status')
+    ]
+
+
+def test_searches_params_parser_get_filters_by_condition_key_field(dummy_request):
+    from snovault.searches.parsers import ParamsParser
+    dummy_request.environ['QUERY_STRING'] = 'type=Experiment&type=File&field=status'
+    p = ParamsParser(dummy_request)
+    assert p.get_filters_by_condition(key_condition='field') == [
+        ('field', 'status')
+    ]
+
+
+def test_searches_params_parser_get_filters_by_condition_key_type(dummy_request):
+    from snovault.searches.parsers import ParamsParser
+    dummy_request.environ['QUERY_STRING'] = 'type=Experiment&type=File&field=status'
+    p = ParamsParser(dummy_request)
+    assert p.get_filters_by_condition(key_condition='type') == [
+        ('type', 'Experiment'),
+        ('type', 'File')
+    ]
+
+
+def test_searches_params_parser_get_filters_by_condition_value_status(dummy_request):
+    from snovault.searches.parsers import ParamsParser
+    dummy_request.environ['QUERY_STRING'] = 'type=Experiment&type=File&field=status'
+    p = ParamsParser(dummy_request)
+    assert p.get_filters_by_condition(value_condition='status') == [
+        ('type', 'Experiment'),
+        ('type', 'File'),
+        ('field', 'status')
+    ]
