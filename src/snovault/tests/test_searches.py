921811d19bfb7a29a5c7f5dc5acbc8d42b99e9ae
Keenan Graham <keenangraham@stanford.edu>
2019-06-10 17:53:37 -0700

    Add query string test

--- src/snovault/tests/test_searches.py
+++ src/snovault/tests/test_searches.py
@@ -6,3 +6,11 @@ def test_searches_params_parser_init(dummy_request):
     p = ParamsParser(dummy_request)
     assert isinstance(p, ParamsParser)
     assert p._request is dummy_request
+
+
+def test_searches_params_parser_query_string(dummy_request):
+    from snovault.searches.parsers import ParamsParser
+    dummy_request.environ['QUERY_STRING'] = 'type=Experiment'
+    p = ParamsParser(dummy_request)
+    assert 'type' in p._request.params
+    assert p._request.params.getall('type') == ['Experiment']
