7f9006b5fdb9ecfb4db7926fc3a7722adb6163b0
Keenan Graham <keenangraham@stanford.edu>
2019-06-17 14:56:14 -0700

    Update names in test

--- src/snovault/tests/test_searches_fields.py
+++ src/snovault/tests/test_searches_fields.py
@@ -1,10 +1,10 @@
 import pytest
 
 
-def test_searches_fields_abstract_response_field_init():
-    from snovault.elasticsearch.searches.fields import AbstractResponseField
-    arf = AbstractResponseField()
-    assert isinstance(arf, AbstractResponseField)
+def test_searches_fields_response_field_init():
+    from snovault.elasticsearch.searches.fields import ResponseField
+    rf = ResponseField()
+    assert isinstance(rf, ResponseField)
 
 
 def test_searches_fields_basic_search_response_field_init():
