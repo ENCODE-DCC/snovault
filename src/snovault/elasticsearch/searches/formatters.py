20806d0552052734e8fdeb279b202ee3baee22ff
Keenan Graham <keenangraham@stanford.edu>
2019-07-05 12:45:54 -0700

    Wrap in class

--- src/snovault/elasticsearch/searches/formatters.py
+++ src/snovault/elasticsearch/searches/formatters.py
@@ -6,40 +6,46 @@ from .interfaces import TOTAL
 from .interfaces import TYPE_KEY
 
 
-def get_aggregation_field():
-    pass
+
+class AggregationFormatter():
+
+    def __init__(self):
+        pass
+
+    def _get_aggregation_field():
+         pass
 
 
-def get_aggregation_title():
-    pass
+    def _get_aggregation_title():
+        pass
 
 
-def get_aggregation_type():
-    pass
+    def _get_aggregation_type():
+        pass
 
 
-def get_aggregation_results():
-    pass
+    def _get_aggregation_results():
+        pass
 
 
-def get_aggregation_total():
-    pass
+    def _get_aggregation_total():
+        pass
 
 
-def aggregation_is_appeneded():
-    pass
+    def _aggregation_is_appeneded():
+        pass
 
 
-def format_aggregation():
-    return {
-        FIELD_KEY: get_aggregation_field(),
-        TITLE: get_aggregation_title(),
-        TERMS: get_aggregation_results(),
-        TOTAL: get_aggregation_total(),
-        TYPE_KEY: get_aggregation_type(),
-        APPENDED: aggregation_is_appeneded(),
-    }
+    def _format_aggregation():
+        return {
+            FIELD_KEY: get_aggregation_field(),
+            TITLE: get_aggregation_title(),
+            TERMS: get_aggregation_results(),
+            TOTAL: get_aggregation_total(),
+            TYPE_KEY: get_aggregation_type(),
+            APPENDED: aggregation_is_appeneded(),
+        }
 
 
-def format_aggregations():
-    pass
+    def format_aggregations():
+        pass
--- src/snovault/tests/test_searches_formatters.py
+++ src/snovault/tests/test_searches_formatters.py
@@ -1,33 +1,33 @@
 import pytest
 
-def test_searches_formatters_get_aggregation_field():
+def test_searches_formatters_aggregation_formatters_get_aggregation_field():
     assert False
 
 
-def test_searches_formatters_get_aggregation_title():
+def test_searches_formatters_aggregation_formatters_get_aggregation_title():
     assert False
 
 
 
-def test_searches_formatters_get_aggregation_type():
+def test_searches_formatters_aggregation_formatters_get_aggregation_type():
     assert False
 
 
-def test_searches_formatters_get_aggregation_results():
+def test_searches_formatters_aggregation_formatters_get_aggregation_results():
     assert False
 
 
-def test_searches_formatters_get_aggregation_total():
+def test_searches_formatters_aggregation_formatters_get_aggregation_total():
     assert False
 
 
-def test_searches_formatters_aggregation_is_appeneded():
+def test_searches_formatters_aggregation_formatters_aggregation_is_appeneded():
     assert False
 
 
-def test_searches_formatters_format_aggregation():
+def test_searches_formatters_aggregation_formatters_format_aggregation():
     assert False
 
 
-def test_searches_formatters_format_aggregations():
+def test_searches_formatters_aggregation_formatters_format_aggregations():
     assert False
