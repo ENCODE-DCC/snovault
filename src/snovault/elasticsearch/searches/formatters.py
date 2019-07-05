654c0cc3a89c533c81cb9ceaafe659dc62df688c
Keenan Graham <keenangraham@stanford.edu>
2019-07-05 12:21:41 -0700

    Add interfaces

--- src/snovault/elasticsearch/searches/formatters.py
+++ src/snovault/elasticsearch/searches/formatters.py
@@ -1,3 +1,10 @@
+from .interfaces import APPENDED
+from .interfaces import FIELD_KEY
+from .interfaces import TERMS
+from .interfaces import TITLE
+from .interfaces import TOTAL
+from .interfaces import TYPE_KEY
+
 
 def get_aggregation_field():
     pass
@@ -25,7 +32,7 @@ def aggregation_is_appeneded():
 
 def format_aggregation():
     return {
-        FIELD: get_aggregation_field(),
+        FIELD_KEY: get_aggregation_field(),
         TITLE: get_aggregation_title(),
         TERMS: get_aggregation_results(),
         TOTAL: get_aggregation_total(),
