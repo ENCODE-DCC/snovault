b18b62ed148c074edf2724f5f442776089b7b77e
Keenan Graham <keenangraham@stanford.edu>
2019-07-16 18:37:22 -0700

    Add raw response view test

--- src/snowflakes/tests/test_searchv2.py
+++ src/snowflakes/tests/test_searchv2.py
@@ -21,3 +21,9 @@ def test_searchv2_view_no_type(workbook, testapp):
     r = testapp.get('/searchv2/')
     print(r.json)
     assert False
+
+
+def test_searchv2_view_raw_response(workbook, testapp):
+    r = testapp.get('/searchv2_raw/?type=Snowflake')
+    print(r.json)
+    assert False
