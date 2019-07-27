b6e6423e34514ffb0462b2181fe06dc83f8ca3a0
Keenan Graham <keenangraham@stanford.edu>
2019-07-26 19:14:03 -0700

    Add view test

--- src/snowflakes/tests/test_searchv2.py
+++ src/snowflakes/tests/test_searchv2.py
@@ -1,20 +1,21 @@
 import pytest
 
+
 # Use workbook fixture from BDD tests (including elasticsearch)
 from .features.conftest import app_settings, app, workbook
 from webob.multidict import MultiDict
 
 
 def test_searchv2_view(workbook, testapp):
-    r = testapp.get('/searchv2/?type=Snowflake')
-    print(r.json)
-    assert False
-
-
-def test_searchv2_view_facets(workbook, testapp):
-    r = testapp.get('/searchv2/?type=Snowflake')
-    print(r.json['facets'])
-    assert False
+    r = testapp.get(
+        '/searchv2/?type=Snowflake&award=/awards/U41HG006992/&accession=SNOFL000LSQ&status=deleted'
+    )
+    assert r.json['title'] == 'Search'
+    assert len(r.json['@graph']) == 1
+    assert r.json['@graph'][0]['accession'] == 'SNOFL000LSQ'
+    assert r.json['@graph'][0]['status'] == 'deleted'
+    assert 'Snowflake' in r.json['@graph'][0]['@type']
+    assert len(r.json['facets']) == 5
 
 
 def test_searchv2_view_no_type(workbook, testapp):
