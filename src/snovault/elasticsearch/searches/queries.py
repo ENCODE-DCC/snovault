43d88a10107c691bafb32234c2de05ba8b54cc86
Keenan Graham <keenangraham@stanford.edu>
2019-09-17 13:21:35 -0700

    Add more tests

--- src/snovault/elasticsearch/searches/queries.py
+++ src/snovault/elasticsearch/searches/queries.py
@@ -1053,7 +1053,7 @@ class TopHitsQueryFactory(BasicSearchQueryFactory):
             }
         )
 
-    def add_filtered_top_hits_aggregations(self):
+    def add_filtered_top_hits_aggregation(self):
         must, must_not, exists, not_exists = self._make_split_filter_queries(
             params=self._get_post_filters()
         )
@@ -1074,5 +1074,5 @@ class TopHitsQueryFactory(BasicSearchQueryFactory):
 
     def build_query(self):
         super().build_query()
-        self.add_filtered_top_hits_aggregations()
+        self.add_filtered_top_hits_aggregation()
         return self.search
