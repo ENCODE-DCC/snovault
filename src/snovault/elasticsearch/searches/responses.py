af42b8249ff364171664120f86d519f5ecef72e0
keenangraham <keenangraham@gmail.com>
2019-10-23 16:27:13 -0700

    [HOTFIX] SNO-120-stream-large-json (#222)

--- src/snovault/elasticsearch/searches/responses.py
+++ src/snovault/elasticsearch/searches/responses.py
@@ -1,6 +1,11 @@
+import json
+
 from collections import OrderedDict
+from pyramid.response import Response
+from types import GeneratorType
 
 from .decorators import remove_from_return
+from .interfaces import APPLICATION_JSON
 from .mixins import AggsToFacetsMixin
 from .mixins import AggsToMatrixMixin
 from .mixins import AuditAggsToMatrixMixin
@@ -19,6 +24,10 @@ class FieldedResponse:
         self.response_fields = response_fields
         self.validate_response_fields()
 
+    @property
+    def ordered_response(self):
+        return OrderedDict(sorted(self.response.items()))
+
     def validate_response_fields(self):
         from .fields import ResponseField
         for f in self.response_fields:
@@ -30,6 +39,43 @@ class FieldedResponse:
                     )
                 )
 
+    def get_request(self):
+        params_parser = self._meta.get('params_parser')
+        if params_parser:
+            return params_parser._request
+
+    def get_or_create_response(self):
+        request = self.get_request()
+        if request:
+            return request.response
+        return Response()
+
+    def _is_request_from_embed(self):
+        request = self.get_request()
+        if request:
+            return request.__parent__ is not None
+
+    def _is_response_with_generator(self):
+        return any(
+            isinstance(v, GeneratorType)
+            for v in self.response.values()
+        )
+    
+    def _should_stream_response(self):
+        return all(
+            [
+                not self._is_request_from_embed(),
+                self._is_response_with_generator()
+            ]
+        )
+
+    def _response_factory(self):
+        if self._should_stream_response():
+            response = StreamedResponse(self)
+        else:
+            response = InMemoryResponse(self)
+        return response
+
     @remove_from_return(values=[None])
     def render(self):
         '''
@@ -37,8 +83,90 @@ class FieldedResponse:
         '''
         for f in self.response_fields:
             self.response.update(f.render(parent=self))
-        return OrderedDict(sorted(self.response.items()))
+        return self._response_factory().render()
 
+
+class StreamedResponse:
+    '''
+    Streams FieldedResponse generators that would otherwise run the machine
+    out of memory.
+    '''
+
+    def __init__(self, fielded_response):
+        self.fielded_response = fielded_response
+
+    def _start_dict(self):
+        return '{'
+
+    def _end_dict(self):
+        return '}'
+
+    def _start_list(self):
+        return '['
+
+    def _end_list(self):
+        return ']'
+
+    def _comma(self):
+        return ','
+
+    def _colon(self):
+        return ':'
+
+    def _to_json(self, value):
+        return json.dumps(value)
+
+    def _to_json_from_generator(self, generator):
+        yield self._start_list()
+        for i, v in enumerate(generator):
+            if i > 0:
+                yield self._comma()
+            yield self._to_json(v)
+        yield self._end_list()
+
+    def _iter(self):
+        yield self._start_dict()
+        for i, (k, v) in enumerate(self.fielded_response.ordered_response.items()):
+            if i > 0:
+                yield self._comma()
+            yield self._to_json(k)
+            yield self._colon()
+            if isinstance(v, GeneratorType):
+                yield from self._to_json_from_generator(v)
+            else:
+                yield self._to_json(v)
+        yield self._end_dict()
+
+    def __iter__(self):
+        yield from (
+            s.encode('utf-8')
+            for s in self._iter()
+        )
+
+    def _make_streamed_response(self):
+        response = self.fielded_response.get_or_create_response()
+        response.content_type=APPLICATION_JSON
+        response.app_iter=self
+        return response
+
+    def render(self):
+        return self._make_streamed_response()
+
+
+class InMemoryResponse:
+    '''
+    Renders FieldedResponse generators in memory.
+    '''
+
+    def __init__(self, fielded_response):
+        self.fielded_response = fielded_response
+
+    def render(self):
+        return {
+            k: list(v) if isinstance(v, GeneratorType) else v
+            for k, v in self.fielded_response.ordered_response.items()
+        }
+    
 
 class QueryResponse:
     '''
