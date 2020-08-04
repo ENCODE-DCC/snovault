02e44779fef16be07e0faed96e79055ac89ef983
Casey Litton <caseylitton@gmail.com>
2020-08-04 10:42:45 -0700

    Have test fail if graphviz not installed

--- src/snowflakes/tests/test_graph.py
+++ src/snowflakes/tests/test_graph.py
@@ -6,5 +6,11 @@ def test_graph_dot(testapp):
 
 def test_graph_svg(testapp):
     res = testapp.get('/profiles/graph.svg', status=200)
+    res_json = res.json
+    if res_json.get('status_code') == 404:
+        msg = res_json.get('message')
+        assert msg == 'graph.svg is not available'
+        # Force fail since graphviz is not installed on the system
+        assert False
     assert res.content_type == 'image/svg+xml'
     assert res.text
