681534411330fc4f0f31a34436b6268ce17bdf5b
Paul Sud <paulsud@stanford.edu>
2021-03-16 13:54:37 -0700

    fix redis charset

--- src/snovault/local_storage.py
+++ src/snovault/local_storage.py
@@ -25,7 +25,7 @@ class LocalStoreClient():
     def __init__(self, **kwargs):
         self.local_tz = kwargs.get('local_tz', 'GMT')
         self.client = StrictRedis(
-            charset='utf-8',
+            encoding='utf-8',
             decode_responses=True,
             db=kwargs['db_index'],
             host=kwargs['host'],
