64bb6d5b0328a74ce4e20ed7bb2d01c2a42e8302
Keenan Graham <keenangraham@stanford.edu>
2021-09-07 13:20:38 -0700

    Update

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ INSTALL_REQUIRES = [
     "redis==3.5.3",
     "requests==2.22.0",
     "simplejson==3.17.0",
-    "snovault-search @ git+https://github.com/ENCODE-DCC/snovault-search.git@SNO-211-increase-batch-size-in-scroll-api",
+    "snovault-search@git+https://github.com/ENCODE-DCC/snovault-search.git@SNO-211-increase-batch-size-in-scroll-api",
     "transaction==3.0.0",
     "venusian==3.0.0",
     "zope.interface==5.3.0",
