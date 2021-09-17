9d1fa265907db046f7efcfb99a5c86d292b03942
Keenan Graham <keenangraham@stanford.edu>
2021-09-17 16:27:04 -0700

    Update

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 import os
 from setuptools import setup, find_packages
 
-
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
