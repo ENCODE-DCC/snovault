9b336739edd155a96d6d5fab7c869c9b8a28c60e
Keenan Graham <keenangraham@stanford.edu>
2021-10-25 10:14:39 -0700

    Point to new snosearch release

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ INSTALL_REQUIRES = [
     "redis==3.5.3",
     "requests==2.22.0",
     "simplejson==3.17.0",
-    "snovault-search @ git+https://github.com/ENCODE-DCC/snovault-search.git@register-search-config-pieces-from-item",
+    "snovault-search==1.0.4",
     "transaction==3.0.0",
     "venusian==3.0.0",
     "zope.interface==5.3.0",
